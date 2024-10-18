75a4ba15e270dcd3eaa4a59a451baa7515275bf3
SarahPerson <sarah@sarahperson.com>
2024-10-18 15:34:39 -0700

    Add dynamic coloring to navigation pane

--- src/pages/index.js
+++ src/pages/index.js
@@ -130,10 +130,10 @@ const links = [
 const IndexPage = () => {
   return (
     <Layout pageTitle="Hi There!">
-      <p>I am a software developer, amateur analog photographer, endurance athelete, and lover of my cat Percy (oh and also my husband Mitch). I am currently working at Microsoft in Azure DNS where I support the Linux based authoritative serving plane. On this website, you can learn more about me and my professional experience, read through various thoughts I have explored, and see some of my various projects. 
+      <p>I am a software developer, amateur analog photographer, endurance athlete, and lover of my cat Percy (oh and also my husband Mitch). I am currently working at Microsoft in Azure DNS where I support the Linux based authoritative serving plane. On this website, you can learn more about me and my professional experience, read through various thoughts I have explored, and see some of my various projects. 
       <br></br> 
       <br></br> 
-      This website is a work in progress - it likely always be, but right now especially. I hope to carve out this little space on the internet for myself in the hopes that I create and contribute more to the ether instead of consume, consume, <b>consume</b>. If you are interested to see my journey, I hope you check back in here from time to time! </p>
+      This website is a work in progress - it likely always be, but right now especially. I hope to carve out this little space on the internet for myself with the goal that I create and contribute more to the ether instead of consume, consume, <b>consume</b>. If you are interested to see my journey, I hope you check back in here from time to time! </p>
     </Layout>
   )
 }
